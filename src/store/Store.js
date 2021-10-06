a347e0ff203b786702b4d5cb452ca28be7eea336
Chanteng <yrnchanteng@gmail.com>
2021-10-06 06:18:59 -0700

    added store

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,2 +1,7 @@
 import { createStore } from "redux";
-import UserReducer from './reducers/UserReducer'
+import userReducer from "../reducers/UserReducer";
+
+
+const Store = createStore(userReducer)
+
+export default Store;
