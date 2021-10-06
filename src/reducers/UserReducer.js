c09e6e0c06aba224867efd9215a5b11ea92fed4b
Chanteng <yrnchanteng@gmail.com>
2021-10-06 06:14:15 -0700

    installed redux

---
+++ src/reducers/UserReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    users: [
+        {name: "Kwame", email: "kwame@gmail.com"},
+        {name: "Kwame", email: "kwame@gmail.com"},
+        {name: "Kwame", email: "kwame@gmail.com"}
+    ]
+} 
+
+const userReducer = (state = initialState, action) => {
+    return state
+}
+
+export default userReducer;
