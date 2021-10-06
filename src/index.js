95d279dec56e76d8f6585ea8595a6a20466a5334
Chanteng <yrnchanteng@gmail.com>
2021-10-06 06:39:25 -0700

    imported provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Store from "./store/Store"
+import { Provider } from 'react-redux';
+
+
 
 ReactDOM.render(
+  <Provider store = {Store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
