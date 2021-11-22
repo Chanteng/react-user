0df5edf2e7ee61c35725da54cd6925e02bd35d9c
Chanteng <nanaquamichanteng@gmail.com>
2021-11-22 09:01:54 -0800

    error fixed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserForm from "./Components/UserForm";
 import UserList from "./Components/UserList";
 import "./App.css"
+import {getAllUsers} from "./actions/userAction"
+import {connect} from "react-redux"
 
-function App() {
+
+
+function App(props) {
   const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    props.getAllUsers();
+  }, [])
 
-  // // function addUser(user) {
-  // //   setUsers([...users, user]);
-  // // }
-
-
-  // function editUser(newDetails, userId) {
-  //   const editedUser = users.map((user) => {
-  //     if (userId === user.id) {
-  //       return newDetails;
-  //     } else {
-  //       return user;
-  //     }
-  //   });
-
-  //   setUsers(editUser);
-  // }
-
-
-  // function deleteUser(userId) {
-  //   //loops through the users state and removes the user with the same id
-  //   const filteredUsers = users.filter((user) => {
-  //     return userId !== user.id;
-  //   });
-
-  //   //sets the users state to the filtered users array
-  //   setUsers(filteredUsers);
-  // }
 
   return (
     <div>
@@ -42,4 +22,9 @@ function App() {
   );
 }
 
-export default App;
+const mapDispatchToProps = {
+	getAllUsers,
+};
+
+
+export default connect(null, mapDispatchToProps) (App);
