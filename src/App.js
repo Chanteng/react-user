2ef011cc7aa4c0f1129d557ed993a740d52b17f9
Chanteng <nanaquamichanteng@gmail.com>
2021-11-22 12:54:43 -0800

    added logout from google

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './Pages/Login'
 import Register from "./Pages/Register"
 import ProtectRoute from "./Components/ProtectRoute"
 
+
 function App () {
     return (
         <BrowserRouter>
@@ -13,6 +14,7 @@ function App () {
             <Route exact path="/login" component={Login}/>  
         </BrowserRouter>
     )
+
 }
 
 export default App
